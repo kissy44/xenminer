996beaef7715e1afec03b043648735799352e8f1
kissy <60071985+kissy44@users.noreply.github.com>
2023-11-07 14:50:40 +0100

    Update vash.sh

--- vast/vash.sh
+++ vast/vash.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xCbFdb56d42a324cEA11C41D187723a6d5e8609eE/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x139a2d77D8A510CADF7C10612F45EF59b7FBa8ef/g' config.conf
 sleep 5
 echo "---------------------------"
 
